73adfb5f9e764bec1cfda11345626e3c4684f76b
Gustavo Santos <gfdsantos@inf.ufpel.edu.br>
2019-04-19 11:35:41 -0300

    fix animations

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navigation(props) {
+  const [selected, setSelected] = useState("");
+
   return (
     <div className="main-navigation">
       <div>
-        <i className="fas fa-home" />
+        <div
+          className={selected === "home" ? "active" : ""}
+          onClick={() => setSelected("home")}
+        >
+          <i className="fas fa-home" />
+          <p>Home</p>
+        </div>
       </div>
       <div>
-        <i className="far fa-clock" />
+        <div
+          className={selected === "logger" ? "active" : ""}
+          onClick={() => setSelected("logger")}
+        >
+          <i className="far fa-clock" />
+          <p>Logger</p>
+        </div>
       </div>
       <div>
-        <i className="far fa-folder-open" />
+        <div
+          className={selected === "docs" ? "active" : ""}
+          onClick={() => setSelected("docs")}
+        >
+          <i className="far fa-folder-open" />
+          <p>Docs</p>
+        </div>
       </div>
       <div>
-        <i className="fas fa-bars" />
+        <div
+          className={selected === "menu" ? "active" : ""}
+          onClick={() => setSelected("menu")}
+        >
+          <i className="fas fa-bars" />
+          <p>Menu</p>
+        </div>
       </div>
     </div>
   );
